8a89b75fac7150ed204c9f76c38f5d99fdc855d2
JustStewartt <31228677+JustStewartt@users.noreply.github.com>
2021-10-29 22:15:05 -0500

    Netflix Clone v1.0

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "4d88fe0a41c1a3578dc2e9d972039428";
+
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
